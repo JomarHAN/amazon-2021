d0d06fa46103fd9316af94aaea48329bc737ea89
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-03-08 15:53:19 -0500

    create nodejs backend

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+const data = {
+    products: [
+        {
+            _id: 1,
+            image: '/images/p1.jpg',
+            name: "Nike Slim Shirt",
+            rating: 4,
+            numReviews: 10,
+            price: 100,
+            countInStock: 10,
+            description: "Best Sell",
+        },
+        {
+            _id: 2,
+            image: '/images/p2.jpg',
+            name: "Adidas Spangled Shirt",
+            rating: 3.5,
+            numReviews: 20,
+            price: 80,
+            countInStock: 10,
+            description: "Best Sell",
+        },
+        {
+            _id: 3,
+            image: '/images/p3.jpg',
+            name: "Puma Blue Shirt",
+            rating: 5,
+            numReviews: 160,
+            price: 65,
+            countInStock: 10,
+            description: "Very Good",
+        },
+        {
+            _id: 4,
+            image: '/images/p4.jpg',
+            name: "Adidas White Kaki Pants",
+            rating: 4,
+            numReviews: 10,
+            price: 70,
+            countInStock: 10,
+            description: "Best Sell",
+        },
+        {
+            _id: 5,
+            image: '/images/p5.jpg',
+            name: "Nike Black Pants",
+            rating: 5,
+            numReviews: 0,
+            price: 120,
+        },
+        {
+            _id: 6,
+            image: '/images/p6.jpg',
+            name: "Puma Gray Pants",
+            rating: 4.5,
+            numReviews: 40,
+            price: 110,
+            countInStock: 10,
+            description: "Best Sell",
+        },
+    ]
+}
+
+export default data;
