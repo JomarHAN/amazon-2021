fa05d501f6b61425018f31c8591b6c08fc126d25
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-03-08 16:31:09 -0500

    load product from backend

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,4 @@ app.get('/', (req, res) => {
 })
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Server is running'))
+app.listen(port, () => console.log(`Server is running on localhost:${port}`))
--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Products from "../components/Products";
-import data from "../data";
+import Axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 function HomeScreen() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await Axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(products);
   return (
     <div>
-      <div className="row center">
-        {data.products.map((product) => (
-          <Products key={product._id} product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.products.map((product) => (
+            <Products key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
