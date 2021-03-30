65c3cafb60b94321ff2fec51b60733d93900c045
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-03-30 17:09:19 -0400

    template barchart

--- frontend/src/Screens/DashboardScreen.jsx
+++ frontend/src/Screens/DashboardScreen.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
 import { useDispatch, useSelector } from "react-redux";
 import { getDashboardCardsInfo } from "../actions/dashboardActions";
 import { getOrderList } from "../actions/orderActions";
@@ -25,6 +26,45 @@ function DashboardScreen(props) {
       dispatch(getDashboardCardsInfo(today));
     }
   }, [dispatch, today, orders, isDashboard]);
+
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
 
   return (
     <div>
@@ -55,7 +95,20 @@ function DashboardScreen(props) {
           subNotDone={cardDashboard.totalOrders - cardDashboard.deliveredOrders}
         />
       </div>
-      <div className="row"></div>
+      <div className="row">
+        <div className="tableChart-dashboard">
+          <h3>Income</h3>
+          <Bar data={data} options={options} />
+        </div>
+        <div className="tableChart-dashboard">
+          <h3>Orders</h3>
+          <Bar data={data} options={options} />
+        </div>
+        <div className="tableChart-dashboard">
+          <h3>Products Trending</h3>
+          <Bar data={data} options={options} />
+        </div>
+      </div>
     </div>
   );
 }
