53202a873a61ed32be4a38387d8c1fff5d459ea6
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-03-31 22:20:16 -0400

    fixed style change week

--- frontend/src/Screens/WeekDashboardScreen.jsx
+++ frontend/src/Screens/WeekDashboardScreen.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import WeeklyChart from "./WeekIncomeChart";
 
 function WeekDashboardScreen() {
+  const d = new Date();
+  const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
+  const mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(d);
+  const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+  console.log(d.getDate());
   return (
     <div className="weekly">
-      <div className="row change-week">
-        <button>
-          <i className="fa fa-caret-left"></i>
-        </button>
-        <h1>03-29-2021</h1>-<h1>04-04-2021</h1>
-        <button>
-          <i className="fa fa-caret-right"></i>
-        </button>
+      <div className="row">
+        <div className="change-week">
+          <button>
+            <i className="fa fa-caret-left"></i>
+          </button>
+          <h1>03/29/2021</h1> - <h1>04/04/2021</h1>
+          <button>
+            <i className="fa fa-caret-right"></i>
+          </button>
+        </div>
       </div>
       <div className="row">
         <WeeklyChart title="Income" />
