c6a4f97d95bb12631d639b793be6ae321efe6384
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-04-19 15:14:31 -0400

    setup socket for server side

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import productRouter from './routers/productRouters.js'
 import path from 'path'
 import uploadRouter from './routers/uploadRouter.js'
 import draftRouter from './routers/draftRouter.js'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 app.use(express.json())
@@ -48,5 +50,48 @@ app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
 })
 
+const serverHTTP = http.Server(app)
+const io = new Server(serverHTTP, { cors: { origin: '*' } })
+const users = []
+
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        const user = users.find(user => user.socketId === socket.id)
+        if (user) {
+            user.online = false
+            console.log(`${user.name} was offline`)
+            const admin = users.find(user => user.isAdmin && user.online)
+            if (admin) {
+                socket.to(admin.socketId).emit('userStatus', user)
+            }
+        }
+    })
+
+    socket.on('onLogin', (user) => {
+        const userStatus = {
+            ...user,
+            online: true,
+            socketId: socket.id,
+            messages: []
+        }
+        const existUser = users.find(x => x._id === userStatus._id)
+        if (existUser) {
+            existUser.online = true,
+                existUser.socketId = socket.id
+        } else {
+            users.push(userStatus)
+        }
+        console.log(`${userStatus.name} is online`)
+        const admin = users.find(x => x.isAdmin && x.online)
+        if (admin) {
+            socket.to(admin.socketId).emit('userStatus', userStatus)
+        }
+        if (userStatus.isAdmin) {
+            socket.to(userStatus.socketId).emit('list-users', users)
+        }
+    })
+})
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on localhost:${port}`))
+serverHTTP.listen(port, () => console.log(`Server IO is running on localhost:${port}`))
+// app.listen(port, () => console.log(`Server is running on localhost:${port}`))
