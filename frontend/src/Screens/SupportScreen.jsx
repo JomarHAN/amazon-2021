6414e430b8edb042068e3a7c6894c54284a6941d
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-04-20 18:42:39 -0400

    keep fixng deploy

--- frontend/src/Screens/SupportScreen.jsx
+++ frontend/src/Screens/SupportScreen.jsx
@@ -5,7 +5,7 @@ import socketIOClient from "socket.io-client";
 
 const ENDPOINT =
   window.location.host.indexOf("localhost") >= 0
-    ? "https://amazon-2021.herokuapp.com/"
+    ? "http://127.0.0.1:5000"
     : window.location.host;
 console.log(window.location);
 let listUsers = [];
--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -3,7 +3,7 @@ import socketIOClient from "socket.io-client";
 
 const ENDPOINT =
   window.location.host.indexOf("localhost") >= 0
-    ? "https://amazon-2021.herokuapp.com/"
+    ? "http://127.0.0.1:5000"
     : window.location.host;
 
 export default function ChatBox({ userInfo }) {
